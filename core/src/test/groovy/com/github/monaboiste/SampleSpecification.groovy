2ca895ebd1364fa92dd94486c5474635ad1e991a
monaboiste <monikaszumna4@gmail.com>
2026-08-25 01:16:42 +0200

    chore(build): scaffold project structure

---
+++ core/src/test/groovy/com/github/monaboiste/SampleSpecification.groovy
@@ -0,0 +1,11 @@
+package com.github.monaboiste
+
+import spock.lang.Specification
+
+class SampleSpecification extends Specification {
+
+    def "test it" () {
+        expect:
+        1 == 1
+    }
+}
